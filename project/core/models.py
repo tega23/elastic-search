31348aad151b3159fd8dd76e60986ba2de9d2fc2
tega23 <tega23@yahoo.com>
2018-03-30 23:15:12 +0100

    Added indexes for all django models

--- project/core/models.py
+++ project/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from .search import StudentIndex,UniversityIndex,CourseIndex
 import django.db.models.options as options
 options.DEFAULT_NAMES = options.DEFAULT_NAMES + (
     'es_index_name', 'es_type_name', 'es_mapping'
@@ -8,11 +9,31 @@ options.DEFAULT_NAMES = options.DEFAULT_NAMES + (
 # Create your models here.
 class University(models.Model):
     name = models.CharField(max_length =255)
+    def indexing(self):
+        obj = UniversityIndex(meta={'id': self.id},name = self.name)
+        obj.save()
+        return obj.to_dict(include_meta=True)
 
 class Course(models.Model):
     name = models.CharField(max_length = 255, unique = True)
+    def indexing(self):
+        obj = CourseIndex(meta={'id': self.id},name = self.name)
+        obj.save()
+        return obj.to_dict(include_meta=True)   
 
 class Student(models.Model):
+    YEAR_IN_SCHOOL_CHOICES =(
+        ('FR','Freshman'),
+        ('SO','Sophomore'),
+        ('JR','Junior'),
+        ('SR','Senior'),
+    )
+    year_in_school = models.CharField(max_length =2, choices = YEAR_IN_SCHOOL_CHOICES)
+    age = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    first_name = models.CharField(max_length =50)
+    last_name = models.CharField(max_length = 50)
+    university = models.ForeignKey(University, null= True, blank = True,on_delete = models.SET_NULL)
+    courses = models.ManyToManyField(Course, null = True, blank = True )
     # Add indexing method to BlogPost
     def indexing(self):
         obj = StudentIndex(
@@ -26,17 +47,3 @@ class Student(models.Model):
         )
         obj.save()
         return obj.to_dict(include_meta=True)
-    class Meta:
-        pass
-    YEAR_IN_SCHOOL_CHOICES =(
-        ('FR','Freshman'),
-        ('SO','Sophomore'),
-        ('JR','Junior'),
-        ('SR','Senior'),
-    )
-    year_in_school = models.CharField(max_length =2, choices = YEAR_IN_SCHOOL_CHOICES)
-    age = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
-    first_name = models.CharField(max_length =50)
-    last_name = models.CharField(max_length = 50)
-    university = models.ForeignKey(University, null= True, blank = True,on_delete = models.SET_NULL)
-    courses = models.ManyToManyField(Course, null = True, blank = True )
--- project/core/search.py
+++ project/core/search.py
@@ -1,5 +1,5 @@
 from elasticsearch_dsl.connections import connections
-from elasticsearch_dsl import DocType, Text, Date,Integer
+from elasticsearch_dsl import DocType, Text, Date,Integer,Search
 from elasticsearch.helpers import bulk
 from elasticsearch import Elasticsearch
 from . import models
@@ -11,13 +11,41 @@ class StudentIndex(DocType):
     class Meta:
         index = 'student-index'
     year_in_school = Text()
-    age = Iteger()
+    age = Integer()
     first_name = Text()
     last_name = Text()
     university = Text()
     courses = Text()
 
+class UniversityIndex(DocType):
+    class Meta:
+        index = 'university-index'
+    name = Text(),
+
+
+class CourseIndex(DocType):
+    class Meta:
+        index = 'course-index'
+    name = Text()
+
 def bulk_indexing():
     StudentIndex.init()
+    UniversityIndex.init()
+    CourseIndex.init()
     es = Elasticsearch()
     bulk(client=es, actions=(b.indexing() for b in models.Student.objects.all().iterator()))
+    bulk(client=es, actions=(b.indexing() for b in models.University.objects.all().iterator()))
+    bulk(client=es, actions=(b.indexing() for b in models.Course.objects.all().iterator()))
+
+
+"""
+def search(age):
+    s = Search().filter('term', age = age)
+    response = s.execute()
+    return response
+
+"""
+def search(university):
+    s = Search().filter('term', university = university)
+    response = s.execute()
+    return response
